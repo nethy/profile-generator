b8b8235de0116dedcfbcdb4bd1b0d26855201b6b
nethy <nethy@vipmail.hu>
2021-04-28 21:57:28 +0200

    finalize equalizer

--- profile_generator/functions/equalizer.py
+++ profile_generator/functions/equalizer.py
@@ -25,16 +25,20 @@ class EqPoint:
             and self._eq(self.right, other.right)
         )
 
-    def _eq(self, a: float, b: float) -> bool:
+    @staticmethod
+    def _eq(a: float, b: float) -> bool:
         return abs(a - b) < _PRECISION
 
 
 def equalize(*points: Point) -> list[EqPoint]:
+    if len(points) == 0:
+        return []
     result = [EqPoint(p.x, p.y, 0, 0) for p in points]
     for i in range(len(result) - 1):
         strength = _get_slope_strength(result[i], result[i + 1])
-        result[i].right = strength
-        result[i + 1].left = strength
+        _set_slope_strength(result[i], result[i + 1], strength)
+    strength = _get_slope_strength(result[-1], _transpose(points[0]))
+    _set_slope_strength(result[-1], result[0], strength)
     return result
 
 
@@ -42,4 +46,13 @@ def _get_slope_strength(a: EqPoint, b: EqPoint) -> float:
     if a.x == b.x or a.y == b.y:
         return 0
     slope = abs((b.y - a.y) / (b.x - a.x))
-    return (1 - math.exp(-2 * slope)) / 2
+    return (1 - 1 / math.exp(2 * slope)) / 2
+
+
+def _set_slope_strength(left: EqPoint, right: EqPoint, strength: float) -> None:
+    left.right = strength
+    right.left = strength
+
+
+def _transpose(point: Point) -> EqPoint:
+    return EqPoint(point.x + 1, point.y, 0, 0)
--- profile_generator/functions/equalizer_test.py
+++ profile_generator/functions/equalizer_test.py
@@ -4,6 +4,9 @@ from .equalizer import EqPoint, Point, equalize
 
 
 class EqualizerTest(TestCase):
+    def test_equalize_no_point(self) -> None:
+        self.assertEqual([], equalize())
+
     def test_equalize_linear(self) -> None:
         points = equalize(Point(0, 0.5), Point(1, 0.5))
         self.assertEqual([EqPoint(0, 0.5, 0, 0), EqPoint(1, 0.5, 0, 0)], points)
@@ -16,9 +19,8 @@ class EqualizerTest(TestCase):
 
         self.assertEqual(
             [
-                EqPoint(0, 1, 0, 0.49084),
-                EqPoint(0.5, 0, 0.49084, 0.49084),
-                EqPoint(1, 1, 0.49084, 0),
+                EqPoint(0, 1, 0.49983, 0.46526),
+                EqPoint(0.75, 0, 0.46526, 0.49983),
             ],
-            equalize(Point(0, 1), Point(0.5, 0), Point(1, 1)),
+            equalize(Point(0, 1), Point(0.75, 0)),
         )
