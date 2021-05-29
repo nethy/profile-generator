9e2808ee0b8a0a2f42eb0c0e0ae2a04440b9f610
Gabor Nemeth <nethy@vipmail.hu>
2021-05-29 22:44:41 +0200

    refactor spline interpolation (#42)

--- profile_generator/model/spline.py
+++ profile_generator/model/spline.py
@@ -1,6 +1,8 @@
 import bisect
-import math
-from collections.abc import Callable, Mapping, Sequence
+from collections.abc import Callable, Sequence
+from math import pi
+
+from profile_generator.unit.precision import equals
 
 Matrix = list[list[float]]
 Vector = list[float]
@@ -9,15 +11,14 @@ Point = tuple[float, float]
 EPSILON = 1 / 256
 
 
-def spline_of(fn: Callable[[float], float]) -> Sequence[Point]:
+def fit(fn: Callable[[float], float]) -> Sequence[Point]:
     ref_values = [fn(x / 255) for x in range(256)]
     points: list[Point] = [(0, ref_values[0]), (1, ref_values[-1])]
     for _ in range(3, 24 + 1):
         spline = interpolate(points)
         max_diff, point = _find_max_diff(ref_values, spline)
         if max_diff > EPSILON:
-            index = bisect.bisect_left(points, point)
-            points.insert(index, point)
+            bisect.insort(points, point)
         else:
             break
     return points
@@ -38,88 +39,84 @@ def _find_max_diff(
 def interpolate(points: Sequence[Point]) -> Callable[[float], float]:
     if len(points) == 0:
         return lambda _: _raise_value_out_of_domain_error()
-    coefficients, constants = _equations(points)
-    solution = solve(coefficients, constants)
-    interpolation = {
-        (points[i][0], points[i + 1][0]): solution[i * 4 : (i + 1) * 4]
-        for i in range(len(points) - 1)
-    }
-    return lambda x: value_of(interpolation, x)
+    x_knots, y_knots = [x for x, _ in points], [y for _, y in points]
+    coefficients, values = _equations(x_knots, y_knots)
+    solution = solve(coefficients, values)
+    return lambda x: _spline(x, x_knots, solution)
 
 
-def _equations(points: Sequence[Point]) -> tuple[Matrix, Vector]:
-    coefficients = []
-    constants = []
-    n = len(points)
+def _equations(x_knots: Vector, y_knots: Vector) -> tuple[Matrix, Vector]:
+    n = len(x_knots)
+    coefficients = [[0.0 for _ in range((n - 1) * 4)] for _ in range((n - 1) * 4)]
+    values = [0.0 for _ in range((n - 1) * 4)]
+    _fn_equations(coefficients, values, x_knots, y_knots)
+    _dx_equations(coefficients, x_knots)
+    _second_dx_equations(coefficients, x_knots)
+    _boundaries(coefficients, x_knots)
+    return (coefficients, values)
+
+
+def _fn_equations(
+    coefficients: Matrix, values: Vector, x_knots: Vector, y_knots: Vector
+) -> None:
+    n = len(x_knots)
     for i in range(n - 1):
-        coeffs, y = _equation(points[i], i, n)
-        coefficients.append(coeffs)
-        constants.append(y)
-        coeffs, y = _equation(points[i + 1], i, n)
-        coefficients.append(coeffs)
-        constants.append(y)
-    for i in range(1, n - 1):
-        coeffs, y = _dx_equation(points[i], i - 1, n)
-        coefficients.append(coeffs)
-        constants.append(y)
-    for i in range(1, n - 1):
-        coeffs, y = _second_dx_equation(points[i], i - 1, n)
-        coefficients.append(coeffs)
-        constants.append(y)
-    coeffs, y = _second_dx_is_zero(points[0], 0, n)
-    coefficients.append(coeffs)
-    constants.append(y)
-    coeffs, y = _second_dx_is_zero(points[n - 1], n - 2, n)
-    coefficients.append(coeffs)
-    constants.append(y)
-    return (coefficients, constants)
+        x, y = x_knots[i], y_knots[i]
+        coefficients[i * 2][i * 4 : (i + 1) * 4] = [x ** 3, x ** 2, x, 1]
+        values[i * 2] = y
+        x, y = x_knots[i + 1], y_knots[i + 1]
+        coefficients[i * 2 + 1][i * 4 : (i + 1) * 4] = [x ** 3, x ** 2, x, 1]
+        values[i * 2 + 1] = y
 
 
-def _equation(point: Point, i: int, n: int) -> tuple[Vector, float]:
-    x, y = point
-    pos = i * 4
-    row: list[float] = [0] * ((n - 1) * 4)
-    row[pos : pos + 4] = [x ** 3, x ** 2, x, 1]
-    return (row, y)
+def _dx_equations(coefficients: Matrix, x_knots: Vector) -> None:
+    n = len(x_knots)
+    for i in range(n - 2):
+        x = x_knots[i + 1]
+        offset = 2 * (n - 1)
+        coefficients[offset + i][i * 4 : (i + 2) * 4] = [
+            3 * x ** 2,
+            2 * x,
+            1,
+            0,
+            -3 * x ** 2,
+            -2 * x,
+            -1,
+            0,
+        ]
 
 
-def _dx_equation(point: Point, i: int, n: int) -> tuple[Vector, float]:
-    x, _ = point
-    pos = i * 4
-    next_pos = (i + 1) * 4
-    row: list[float] = [0] * ((n - 1) * 4)
-    row[pos : pos + 3] = [3 * x ** 2, 2 * x, 1]
-    row[next_pos : next_pos + 3] = [-3 * x ** 2, -2 * x, -1]
-    return (row, 0)
+def _second_dx_equations(coefficients: Matrix, x_knots: Vector) -> None:
+    n = len(x_knots)
+    for i in range(n - 2):
+        x = x_knots[i + 1]
+        offset = 3 * (n - 1) - 1
+        coefficients[offset + i][i * 4 : (i + 2) * 4] = [
+            6 * x,
+            2,
+            0,
+            0,
+            -6 * x,
+            -2,
+            0,
+            0,
+        ]
 
 
-def _second_dx_equation(point: Point, i: int, n: int) -> tuple[Vector, float]:
-    x, _ = point
-    pos = i * 4
-    next_pos = (i + 1) * 4
-    row: list[float] = [0] * ((n - 1) * 4)
-    row[pos : pos + 2] = [6 * x, 2]
-    row[next_pos : next_pos + 2] = [-6 * x, -2]
-    return (row, 0)
+def _boundaries(coefficients: Matrix, x_knots: Vector) -> None:
+    n = len(x_knots)
+    x = x_knots[0]
+    coefficients[-2][:2] = [6 * x, 2]
+    x = x_knots[-1]
+    coefficients[-1][(n - 2) * 4 : (n - 2) * 4 + 2] = [6 * x, 2]
 
 
-def _second_dx_is_zero(point: Point, i: int, n: int) -> tuple[Vector, float]:
-    x, _ = point
-    pos = i * 4
-    row: list[float] = [0] * ((n - 1) * 4)
-    row[pos : pos + 2] = [6 * x, 2]
-    return (row, 0)
-
-
-def value_of(
-    interpolation: Mapping[tuple[float, float], Sequence[float]], x: float
-) -> float:
-    items = sorted(interpolation.items())
-    for item in items:
-        (low, high), coeffs = item
-        if low <= x <= high:
-            return coeffs[0] * x ** 3 + coeffs[1] * x ** 2 + coeffs[2] * x + coeffs[3]
-    return _raise_value_out_of_domain_error()
+def _spline(x: float, knots: Sequence[float], coefficients: Sequence[float]) -> float:
+    if not knots[0] <= x <= knots[-1]:
+        _raise_value_out_of_domain_error()
+    index = max(0, bisect.bisect_left(knots, x) - 1)
+    a, b, c, d = coefficients[index * 4 : index * 4 + 4]
+    return a * x ** 3 + b * x ** 2 + c * x + d
 
 
 def _raise_value_out_of_domain_error() -> float:
@@ -132,7 +129,7 @@ def solve(coefficients: Matrix, constants: Vector) -> Vector:
     for pivot_idx in range(rank):
         _swap_row(system, pivot_idx)
         _normalize(system[pivot_idx], pivot_idx)
-        _eliminate_column(system, rank, pivot_idx)
+        _eliminate_column(system, pivot_idx)
 
     return [line[-1] for line in system]
 
@@ -145,11 +142,11 @@ def _system_of(coefficients: Matrix, constants: Vector) -> Matrix:
 
 
 def _swap_row(system: Matrix, pivot_idx: int) -> None:
-    if not math.isclose(system[pivot_idx][pivot_idx], 0):
+    if not equals(system[pivot_idx][pivot_idx], 0):
         return
 
     other_index = pivot_idx
-    while other_index < len(system) and math.isclose(system[other_index][pivot_idx], 0):
+    while other_index < len(system) and equals(system[other_index][pivot_idx], 0):
         other_index += 1
 
     if other_index == len(system):
@@ -159,19 +156,22 @@ def _swap_row(system: Matrix, pivot_idx: int) -> None:
 
 
 def _normalize(line: Vector, pivot_idx: int) -> None:
-    multiplier = 1.0 / line[pivot_idx]
-    for i in range(pivot_idx, len(line)):
-        line[i] = line[i] * multiplier
+    divisor = line[pivot_idx]
+    line[pivot_idx] = 1.0
+    for i in range(pivot_idx + 1, len(line)):
+        line[i] = line[i] / divisor
 
 
-def _eliminate_column(system: Matrix, rank: int, pivot_idx: int) -> None:
+def _eliminate_column(system: Matrix, pivot_idx: int) -> None:
+    rank = len(system)
     for i in range(rank):
         if i == pivot_idx:
             continue
         _eliminate(system[i], system[pivot_idx], pivot_idx)
 
 
-def _eliminate(actual: Vector, pivot_row: Vector, pivot_idx: int) -> None:
-    multiplier = actual[pivot_idx] / pivot_row[pivot_idx]
-    for i in range(pivot_idx, len(actual)):
-        actual[i] = actual[i] - pivot_row[i] * multiplier
+def _eliminate(actual_row: Vector, pivot_row: Vector, pivot_idx: int) -> None:
+    multiplier = actual_row[pivot_idx] / pivot_row[pivot_idx]
+    actual_row[pivot_idx] = 0.0
+    for i in range(pivot_idx + 1, len(actual_row)):
+        actual_row[i] = actual_row[i] - pivot_row[i] * multiplier
