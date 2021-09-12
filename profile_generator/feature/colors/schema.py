4a9b2dd3d9b8b4f241be9f3938098848e56c68e9
Gabor Nemeth <2802922+nethy@users.noreply.github.com>
2021-09-12 21:47:17 +0200

    rework vibrance (#64)

--- profile_generator/feature/colors/schema.py
+++ profile_generator/feature/colors/schema.py
@@ -1,7 +1,9 @@
 from collections.abc import Mapping
 from typing import Any
 
+from profile_generator.model.view import raw_therapee
 from profile_generator.schema import composite_process, object_of, range_of
+from profile_generator.unit import Point
 
 from .hsl import schema as hsl
 from .profile import schema as profile
@@ -9,36 +11,45 @@ from .white_balance import schema as white_balance
 
 _LAB_ENABLED = "LabEnabled"
 _LAB_CHROMACITY = "LabChromacity"
-_LAB_SKIN_PROTECTION = "LabRASTProtection"
+_HSVE_ENABLED = "HSVEEnabled"
+_HSVE_SCURVE = "HSVESCurve"
 
-
-_DEFAULT = {_LAB_ENABLED: "false", _LAB_CHROMACITY: "0", _LAB_SKIN_PROTECTION: "0"}
+_DEFAULT = {
+    _LAB_ENABLED: "false",
+    _LAB_CHROMACITY: "0",
+    _HSVE_ENABLED: "false",
+    _HSVE_SCURVE: raw_therapee.CurveType.LINEAR,
+}
 
 _DEFAULT_VIBRANCE = 0
-_DEFAULT_SKIN_PROTECTION = 0
 
 
 def _process(data: Any) -> Mapping[str, str]:
     vibrance = _get_vibrance(data)
-    skin_protection = _get_skin_protection(data)
-    return _DEFAULT | vibrance | skin_protection
+    return _DEFAULT | vibrance
 
 
 def _get_vibrance(data: Any) -> Mapping[str, str]:
     vibrance = data.get("vibrance", _DEFAULT_VIBRANCE)
-    is_enabled = str(vibrance != 0).lower()
-    return {_LAB_ENABLED: is_enabled, _LAB_CHROMACITY: str(vibrance)}
-
-
-def _get_skin_protection(data: Any) -> Mapping[str, str]:
-    protection = data.get("skin_tone_protection", _DEFAULT_SKIN_PROTECTION)
-    return {_LAB_SKIN_PROTECTION: str(protection)}
+    if vibrance > 0:
+        skin_saturation = 0.5 * (1 + vibrance / 2 / 100)
+        blue_saturation = 0.5 * (1 + vibrance / 100)
+        equalizer = [Point(0.05, skin_saturation), Point(0.55, blue_saturation)]
+        return {
+            _LAB_ENABLED: "true",
+            _LAB_CHROMACITY: str(round(vibrance / 2)),
+            _HSVE_ENABLED: "true",
+            _HSVE_SCURVE: raw_therapee.CurveType.STANDARD
+            + raw_therapee.present_linear_equalizer(equalizer),
+        }
+    else:
+        is_enabled = str(vibrance != 0).lower()
+        return {_LAB_ENABLED: is_enabled, _LAB_CHROMACITY: str(vibrance)}
 
 
 SCHEMA = object_of(
     {
         "vibrance": range_of(-100, 100),
-        "skin_tone_protection": range_of(0, 100),
         "white_balance": white_balance.SCHEMA,
         "hsl": hsl.SCHEMA,
         "profile": profile.SCHEMA,
