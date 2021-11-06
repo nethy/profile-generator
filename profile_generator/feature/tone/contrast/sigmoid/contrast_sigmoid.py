89888475d9a39b43e7b876dcd4e8cc31cbb39e0e
nethy <2802922+nethy@users.noreply.github.com>
2021-11-06 16:40:00 +0100

    replace bezier gamma with flat gamma

--- profile_generator/feature/tone/contrast/sigmoid/contrast_sigmoid.py
+++ profile_generator/feature/tone/contrast/sigmoid/contrast_sigmoid.py
@@ -5,7 +5,7 @@ from functools import cache
 from profile_generator.model import spline
 from profile_generator.model.color import constants, rgb
 from profile_generator.model.color.space import SRGB
-from profile_generator.model.tone_curve import bezier_gamma, tone_curve_filmic
+from profile_generator.model.tone_curve import flat_gamma, tone_curve_filmic
 from profile_generator.unit import Point
 
 
@@ -16,11 +16,11 @@ def calculate(
     brightness: float = 0.0,
 ) -> Sequence[Point]:
     middle = _get_middle(grey18)
-    brightness_curve, correction = bezier_gamma(
+    brightness_curve = flat_gamma(
         rgb.normalize_value(grey18),
         _adjust_ev(rgb.normalize_value(grey18), brightness),
     )
-    curve = tone_curve_filmic(middle, slope * math.sqrt(correction))
+    curve = tone_curve_filmic(middle, slope)
     return [Point(x, y) for x, y in spline.fit(lambda x: curve(brightness_curve(x)))]
 
 
--- profile_generator/model/tone_curve.py
+++ profile_generator/model/tone_curve.py
@@ -2,11 +2,12 @@ import math
 from collections.abc import Callable
 from functools import cache
 
-from profile_generator.unit import Curve, Point
+from profile_generator.unit import Curve, Line, Point
 
-from . import bezier, gamma, sigmoid
+from . import gamma, sigmoid
 
 
+@cache
 def tone_curve_filmic(middle: Point, gradient: float) -> Curve:
     return _tone_curve(middle, gradient, _contrast_curve_filmic)
 
@@ -14,11 +15,11 @@ def tone_curve_filmic(middle: Point, gradient: float) -> Curve:
 def _tone_curve(
     middle: Point, gradient: float, contrast_curve: Callable[[float], Curve]
 ) -> Curve:
-    brightness, correction = bezier_gamma(*middle)
+    brightness = flat_gamma(*middle)
 
     shift_x = gamma.power(middle.y, 0.5)
     shift_y = gamma.power(0.5, middle.y)
-    _contrast = contrast_curve(gradient * math.sqrt(correction))
+    _contrast = contrast_curve(gradient)
     _shifted_contrast = lambda x: shift_y(_contrast(shift_x(x)))
 
     return lambda x: _shifted_contrast(brightness(x))
@@ -27,16 +28,20 @@ def _tone_curve(
 def _contrast_curve_filmic(gradient: float) -> Curve:
     if math.isclose(gradient, 1):
         return lambda x: x
-    shadows = sigmoid.exp(gradient)
-    highlights = sigmoid.linear(gradient)
-    return lambda x: shadows(x) if x < 0.5 else highlights(x)
+    base = sigmoid.exp(gradient)
+    protector = sigmoid.linear(gradient)
+    return lambda x: base(x) if x < 0.5 else (2 * base(x) + protector(x)) / 3
 
 
 @cache
-def bezier_gamma(x: float, y: float) -> tuple[Curve]:
-    points = [(Point(0, 0), 1), (Point(x, y), 1), (Point(1, 1), 1)]
-    base = bezier.curve(points)
-    corrector, gradient = (
-        gamma.exp(base(x), y) if y >= x else gamma.inverse_exp(base(x), y)
-    )
-    return (lambda val: corrector(base(val)), gradient(0))
+def flat_gamma(x: float, y: float) -> Curve:
+    shadow = Line.from_points(Point(0, 0), Point(x, y))
+    highlight = Line.from_points(Point(x, y), Point(1, 1))
+    weight_correction, _ = gamma.linear(x, y)
+    weight_curve = sigmoid.linear(4)
+    weight = lambda val: weight_curve(weight_correction(val))
+    curve = lambda val: (1 - weight(val)) * shadow.get_y(val) + weight(
+        val
+    ) * highlight.get_y(val)
+
+    return curve
