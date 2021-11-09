d358aad1493917580d621fc71a379b72fe42624b
nethy <2802922+nethy@users.noreply.github.com>
2021-11-09 22:47:45 +0100

    rework flat gamma

--- profile_generator/model/tone_curve.py
+++ profile_generator/model/tone_curve.py
@@ -2,7 +2,7 @@ import math
 from collections.abc import Callable
 from functools import cache
 
-from profile_generator.unit import Curve, Line, Point
+from profile_generator.unit import Curve, Point
 
 from . import gamma, sigmoid
 
@@ -31,31 +31,15 @@ def _contrast_curve_filmic(gradient: float) -> Curve:
         return lambda x: x
     base = sigmoid.exp(gradient)
     protector = sigmoid.linear(gradient)
-    return lambda x: base(x) if x < 0.5 else (2 * base(x) + protector(x)) / 3
+    return lambda x: base(x) if x < 0.5 else (base(x) + 3 * protector(x)) / 4
 
 
 @cache
 def flat_gamma(x: float, y: float) -> Curve:
-    if math.isclose(x, y):
-        return lambda val: val
-    shadow = Line.from_points(Point(0, 0), Point(x, y))
-    highlight = Line.from_points(Point(x, y), Point(1, 1))
-    weight_correction, _ = gamma.exp(x, 0.5)
-    weight = lambda val: weight_curve(weight_correction(val))
-    curve = lambda val: (1 - weight(val)) * shadow.get_y(val) + weight(
-        val
-    ) * highlight.get_y(val)
-
+    g = y / x / (1 - y) - 1 / (1 - x)
+    highlight = lambda x: (x + g * x) / (1 + g * x)
     return (
-        lambda val: curve(val)
-        if val > x
-        else (1 - val / x) * shadow.get_y(val) + val / x * curve(val)
+        lambda val: y / x * val
+        if val < x
+        else highlight(val - x) / (1 / (1 - y)) / highlight(1 - x) + y
     )
-
-
-WEIGHT_EXP = sigmoid.exp(1.414213562373095)
-WEIGHT_LINEAR = sigmoid.linear(1.414213562373095)
-
-
-def weight_curve(x: float) -> float:
-    return WEIGHT_EXP(WEIGHT_LINEAR(x))
--- profile_generator/model/tone_curve_test.py
+++ profile_generator/model/tone_curve_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from .tone_curve import Point, tone_curve_filmic
+from .tone_curve import Point, flat_gamma, tone_curve_filmic
 
 _GREY = Point(87 / 255, 119 / 255)
 
@@ -10,7 +10,17 @@ class TestToneCurve(TestCase):
         _curve = tone_curve_filmic(_GREY, 2)
 
         self.assertAlmostEqual(_curve(0), 0)
-        self.assertAlmostEqual(_curve(0.2), 0.15545631)
+        self.assertAlmostEqual(_curve(0.2), 0.14479477)
         self.assertAlmostEqual(_curve(_GREY.x), _GREY.y)
-        self.assertAlmostEqual(_curve(0.8), 0.94079146)
+        self.assertAlmostEqual(_curve(0.8), 0.947612761)
         self.assertAlmostEqual(_curve(1), 1)
+
+    def test_flat_gamma(self) -> None:
+        _gamma = flat_gamma(0.25, 0.5)
+
+        self.assertAlmostEqual(_gamma(0), 0)
+        self.assertAlmostEqual(_gamma(0.1), 0.2)
+        self.assertAlmostEqual(_gamma(0.25), 0.5)
+        self.assertAlmostEqual(_gamma(0.6), 0.8620690)
+        self.assertAlmostEqual(_gamma(0.9), 0.9756098)
+        self.assertAlmostEqual(_gamma(1), 1)
