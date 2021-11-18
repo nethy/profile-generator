889638103abbdbc7ebb88fe2b45e273d0b7faf99
Gabor Nemeth <2802922+nethy@users.noreply.github.com>
2021-11-18 21:11:35 +0100

    tone curve tweaks (#82)

--- profile_generator/model/tone_curve.py
+++ profile_generator/model/tone_curve.py
@@ -1,42 +1,101 @@
 import math
 from collections.abc import Callable
-from functools import cache
 
-from profile_generator.unit import Curve, Point
+from profile_generator.model import bezier
+from profile_generator.unit import Curve, Line, Point
 
-from . import bezier, gamma, sigmoid
+from . import gamma, sigmoid
+
+
+def tone_curve_filmic(middle: Point, gradient: float) -> Curve:
+    return _tone_curve(middle, gradient, _contrast_curve_filmic)
 
 
 def _tone_curve(
     middle: Point, gradient: float, contrast_curve: Callable[[float], Curve]
 ) -> Curve:
-    brightness, correction = bezier_gamma(*middle)
+    brightness = shadow_linear_gamma(*middle)
 
     shift_x = gamma.power(middle.y, 0.5)
     shift_y = gamma.power(0.5, middle.y)
-    _contrast = contrast_curve(gradient * math.sqrt(correction))
+    _contrast = contrast_curve(gradient)
     _shifted_contrast = lambda x: shift_y(_contrast(shift_x(x)))
 
     return lambda x: _shifted_contrast(brightness(x))
 
 
-def tone_curve_filmic(middle: Point, gradient: float) -> Curve:
-    return _tone_curve(middle, gradient, contrast_curve_filmic)
-
-
-def contrast_curve_filmic(gradient: float) -> Curve:
+def _contrast_curve_filmic(gradient: float) -> Curve:
     if math.isclose(gradient, 1):
         return lambda x: x
-    shadows = sigmoid.exp(gradient)
-    highlights = sigmoid.linear(gradient)
-    return lambda x: shadows(x) if x < 0.5 else (shadows(x) + 2 * highlights(x)) / 3
+    shadow = sigmoid.exp(gradient)
+    higlight = sigmoid.linear(gradient)
+    return lambda x: shadow(x) if x < 0.5 else higlight(x)
 
 
-@cache
-def bezier_gamma(x: float, y: float) -> tuple[Curve, float]:
-    points = [(Point(0, 0), 1), (Point(x, y), 3), (Point(1, 1), 1)]
-    base = bezier.curve(points)
-    corrector, gradient = (
-        gamma.exp(base(x), y) if y >= x else gamma.inverse_exp(base(x), y)
+def shadow_linear_gamma(x: float, y: float) -> Curve:
+    g = y / x / (1 - y) - 1 / (1 - x)
+    highlight = lambda x: (x + g * x) / (1 + g * x)
+    return (
+        lambda val: y / x * val
+        if val < x
+        else highlight(val - x) / (1 / (1 - y)) / highlight(1 - x) + y
     )
-    return (lambda val: corrector(base(val)), gradient(0))
+
+
+def interpolated_gamma(x: float, y: float) -> Curve:
+    shadow = Line.from_points(Point(0, 0), Point(x, y))
+    highlight = Line.from_points(Point(x, y), Point(1, 1))
+    shift, _ = gamma.linear(x, 0.5)
+    weight = lambda val: shift((val - x) / (1 - x))
+    return (
+        lambda val: shadow.get_y(val)
+        if val < x
+        else (1 - weight(val)) * shadow.get_y(val) + weight(val) * highlight.get_y(val)
+    )
+
+
+def highlight_linear_gamma(x: float, y: float) -> Curve:
+    shadow, _ = gamma.linear(x, y)
+    return (
+        lambda val: shadow((1 / x) * val) / (1 / y)
+        if val < x
+        else (1 - y) / (1 - x) * val + 1 - (1 - y) / (1 - x)
+    )
+
+
+def flat_gamma(x: float, y: float) -> Curve:
+    shadow = Line.from_points(Point(0, 0), Point(x, y))
+    highlight = Line.from_points(Point(x, y), Point(1, 1))
+    shift, gradient = gamma.linear(x, 0.5)
+    contrast = sigmoid.linear(4 / gradient(x))
+    weight = lambda val: contrast(shift(val))
+    return lambda val: (1 - weight(val)) * shadow.get_y(val) + weight(
+        val
+    ) * highlight.get_y(val)
+
+
+def gradient_matching_linear(x: float, y: float) -> Curve:
+    g = y / x - 1
+    return lambda x: (x + g * x) / (1 + g * x)
+
+
+def bezier_gamma(x: float, y: float) -> Curve:
+    shadow = Line.from_points(Point(0, 0), Point(x, y))
+    highlight = Line.from_points(Point(x, y), Point(1, 1))
+    weight = bezier.curve(
+        [
+            (Point(0, 1), 1),
+            (Point(x, 1), 1),
+            (Point(x, x), 1),
+            (Point(1, 0), 1),
+        ]
+    )
+    return lambda val: weight(val) * shadow.get_y(val) + (
+        1 - weight(val)
+    ) * highlight.get_y(val)
+
+
+def hybrid_linear(x: float, y: float) -> Curve:
+    base = gradient_matching_linear(x, y)
+    correction = shadow_linear_gamma(base(x), y)
+    return lambda val: correction(base(val))
--- profile_generator/model/tone_curve_test.py
+++ profile_generator/model/tone_curve_test.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 
-from .tone_curve import Point, tone_curve_filmic
+from .tone_curve import (
+    Point,
+    interpolated_gamma,
+    shadow_linear_gamma,
+    tone_curve_filmic,
+)
 
 _GREY = Point(87 / 255, 119 / 255)
 
@@ -10,7 +15,27 @@ class TestToneCurve(TestCase):
         _curve = tone_curve_filmic(_GREY, 2)
 
         self.assertAlmostEqual(_curve(0), 0)
-        self.assertAlmostEqual(_curve(0.2), 0.1557757)
+        self.assertAlmostEqual(_curve(0.2), 0.144794775)
         self.assertAlmostEqual(_curve(_GREY.x), _GREY.y)
-        self.assertAlmostEqual(_curve(0.8), 0.9366098)
+        self.assertAlmostEqual(_curve(0.8), 0.939096291)
         self.assertAlmostEqual(_curve(1), 1)
+
+    def test_linear_gamma(self) -> None:
+        _gamma = shadow_linear_gamma(0.25, 0.5)
+
+        self.assertAlmostEqual(_gamma(0), 0)
+        self.assertAlmostEqual(_gamma(0.2), 0.4)
+        self.assertAlmostEqual(_gamma(0.25), 0.5)
+        self.assertAlmostEqual(_gamma(0.3), 0.5882353)
+        self.assertAlmostEqual(_gamma(0.8), 0.9459459)
+        self.assertAlmostEqual(_gamma(1), 1)
+
+    def test_interpolated_gamma(self) -> None:
+        _gamma = interpolated_gamma(0.25, 0.5)
+
+        self.assertAlmostEqual(_gamma(0), 0)
+        self.assertAlmostEqual(_gamma(0.2), 0.4)
+        self.assertAlmostEqual(_gamma(0.25), 0.5)
+        self.assertAlmostEqual(_gamma(0.3), 0.5882353)
+        self.assertAlmostEqual(_gamma(0.8), 0.9459459)
+        self.assertAlmostEqual(_gamma(1), 1)
